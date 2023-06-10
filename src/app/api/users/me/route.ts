cac150006cbfca1538a4e6eddd14380ccd9c3775
Suzan Rana <dev.suzanrana@gmail.com>
2023-06-11 00:59:49 +0545

    refactoring...

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,35 +1,8 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { verifyToken } from "@/lib/jwt";
+import { validateUser } from "@/lib/auth";
 
-export function GET(request: NextRequest) {}
+export function GET(request: NextRequest) {
+  const user = validateUser(request)
+}
 
-const validateUser = (request: NextRequest) => {
-  const [type, token] = request.headers.get("authorization")?.split(" ") || [];
-  if (type !== "Bearer" || !token) {
-    return NextResponse.json(
-      {
-        message: "Unauthorized",
-        error: "Invalid token",
-      },
-      {
-        status: 401,
-      }
-    );
-  }
 
-  try {
-    const decoded = verifyToken(token);
-    return decoded;
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: "Unauthorized",
-        error: "Invalid token",
-      },
-      {
-        status: 401,
-      }
-    );
-  }
-};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { jwtVerify, SignJWT } from "jose";
 import { nanoid } from "nanoid";
-import type { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "./jwt";
 
 interface UserJwtPayload {
   jti: string;
@@ -47,3 +48,36 @@ export async function setUserCookie(res: NextResponse) {
 
   return res;
 }
+
+/*
+Validates user whether they have token or not, and responds with a user-response
+*/
+export const validateUser = (request: NextRequest) => {
+  const [type, token] = request.headers.get("authorization")?.split(" ") || [];
+  if (type !== "Bearer" || !token) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+        error: "Invalid token",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    return decoded;
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+        error: "Invalid token",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+};
