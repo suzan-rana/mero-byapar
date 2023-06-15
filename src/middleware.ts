c099c0c0661e7ac99000eea534eae798e3db29dc
Suzan Rana <dev.suzanrana@gmail.com>
2023-06-15 23:11:18 +0545

    middleware removed.

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-export default function middleware(req: NextRequest) {
-  const [type, token] = req.headers.get("authorization")?.split(" ") || [];
-  return NextResponse.next();
-}
