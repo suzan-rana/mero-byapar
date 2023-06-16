b1d169714db7ba23c00b7182552c9557fdb3d7c2
Suzan Rana <dev.suzanrana@gmail.com>
2023-06-16 23:30:58 +0545

    TYPESCRIPT NOW WORKING...

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,27 +1,39 @@
-import {
-  AiFillCloseCircle,
-  AiOutlineFundProjectionScreen,
-  AiOutlineLogout,
-  AiOutlineUser,
-} from "react-icons/ai";
-import { CgMenuLeftAlt } from "react-icons/cg";
-import { RxDashboard } from "react-icons/rx";
-import { BsCloudDrizzle } from "react-icons/bs";
-import { CiStar } from "react-icons/ci";
-import {
-  MdOutlineFavoriteBorder,
-  MdProductionQuantityLimits,
-} from "react-icons/md";
+// import {
+//   AiFillCloseCircle,
+//   AiOutlineFundProjectionScreen,
+//   AiOutlineLogout,
+//   AiOutlineUser,
+// } from "react-icons/ai";
+// import { CgMenuLeftAlt } from "react-icons/cg";
+// import { RxDashboard } from "react-icons/rx";
+// import { BsCloudDrizzle } from "react-icons/bs";
+// import { CiStar } from "react-icons/ci";
+// import {
+//   MdOutlineFavoriteBorder,
+//   MdProductionQuantityLimits,
+// } from "react-icons/md";
+// const Icons = {
+//   close: AiFillCloseCircle,
+//   menu: <CgMenuLeftAlt className="w-7 h-7 cursor-pointer text-green-800" />,
+//   dashboard: <RxDashboard className="w-5 h-5   cursor-pointer" />,
+//   product: <MdProductionQuantityLimits className="w-5 h-5   cursor-pointer" />,
+//   toBuy: <AiOutlineFundProjectionScreen className="w-5 h-5   cursor-pointer" />,
+//   order: <MdOutlineFavoriteBorder className="w-5 h-5   cursor-pointer" />,
+//   sales: <BsCloudDrizzle className="w-5 h-5   cursor-pointer" />,
+//   team: <CiStar className="w-5 h-5   cursor-pointer" />,
+//   profile: <AiOutlineUser className="w-5 h-5   cursor-pointer" />,
+//   logout: <AiOutlineLogout className="w-5 h-5   cursor-pointer" />,
+// };
 const Icons = {
-  close: AiFillCloseCircle,
-  menu: <CgMenuLeftAlt className="w-7 h-7 cursor-pointer text-green-800" />,
-  dashboard: <RxDashboard className="w-5 h-5   cursor-pointer" />,
-  product: <MdProductionQuantityLimits className="w-5 h-5   cursor-pointer" />,
-  toBuy: <AiOutlineFundProjectionScreen className="w-5 h-5   cursor-pointer" />,
-  order: <MdOutlineFavoriteBorder className="w-5 h-5   cursor-pointer" />,
-  sales: <BsCloudDrizzle className="w-5 h-5   cursor-pointer" />,
-  team: <CiStar className="w-5 h-5   cursor-pointer" />,
-  profile: <AiOutlineUser className="w-5 h-5   cursor-pointer" />,
-  logout: <AiOutlineLogout className="w-5 h-5   cursor-pointer" />,
+  close: <p></p>,
+  menu: <p></p>,
+  dashboard: <p></p>,
+  product: <p></p>,
+  toBuy: <p></p>,
+  order: <p></p>,
+  sales: <p></p>,
+  team: <p></p>,
+  profile: <p></p>,
+  logout: <p></p>,
 };
 export default Icons;
