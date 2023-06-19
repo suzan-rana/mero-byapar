4d9bd12a6dba7316c1391be915ca4bf6959567c1
Suzan Rana <dev.suzanrana@gmail.com>
2023-06-19 20:35:23 +0545

    axios changes

--- src/common/api/axios.ts
+++ src/common/api/axios.ts
@@ -11,7 +11,17 @@ const instance = axios.create({
 
 instance.interceptors.request.use((req) => {
   if (req.method === "post") {
-    toast.loading("SUBMITTING YOUR REQUEST...", {
+    toast.loading("Creating...", {
+      toastId: "LOADING",
+    });
+  }
+  if (req.method === "patch" || req.method === "put") {
+    toast.loading("Updating...", {
+      toastId: "LOADING",
+    });
+  }
+  if (req.method === "delete") {
+    toast.loading("Deleting...", {
       toastId: "LOADING",
     });
   }
@@ -29,8 +39,24 @@ instance.interceptors.response.use(
   },
   (error) => {
     toast.dismiss("LOADING");
-    new Promise((res) => setTimeout(res, 500));
-    if (error.response.status === 401) {
+    new Promise((res) => setTimeout(res, 300));
+    if (
+      error.response.method === "post" ||
+      error.response.method === "put" ||
+      error.response.method === "delete"
+    ) {
+      if (error.response.status === 401) {
+        toast.error(
+          error.response.data.message ||
+            "You are not authorized to perform the action!"
+        );
+      } else {
+        toast.error(
+          error.reponse.data.message || "Somehow request failed, Contact Admin"
+        );
+      }
+    }
+    if (error.response.status === 401 && error.request.method === "get") {
       toast.error(
         error.response.data.message ||
           "You are not authorized to perform the action!"
